06be9e93a01abc3912a52c9000725890b677ad12
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-05-25 07:02:12 +0300

    all works

--- recipe/test/test_tags_api.py
+++ recipe/test/test_tags_api.py
@@ -32,6 +32,7 @@ class PrivateTagsApiTests(TestCase):
         tags=Tag.objects.all().order_by('-name')
         serializer=TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
         user2=get_user_model().objects.create_user(email='other@mail.com', password='pass')
