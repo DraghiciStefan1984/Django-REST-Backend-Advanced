1e3073edd3bdf26b2777b48e97ae2bd8e1a15ae7
draghicistefan <draghicistefan1984@gmail.com>
2020-05-28 08:49:22 +0300

    static and media and upload image

--- core/models.py
+++ core/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, file_name):
+    ext=file_name.split('.')[-1]
+    file_name=f'{uuis.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe/', file_name)
 
 
 # Create your models here.
@@ -54,6 +62,7 @@ class Recipe(models.Model):
     link=models.CharField(max_length=255, blank=True)
     ingredients=models.ManyToManyField(Ingredient)
     tags=models.ManyToManyField(Tag)
+    image=models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
